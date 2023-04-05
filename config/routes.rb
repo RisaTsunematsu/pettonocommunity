6b9c5230ff0383fcbf72b3dd83b7be65419b47cd
EC2 Default User <ec2-user@ip-172-31-21-177.ap-northeast-1.compute.internal>
2023-04-05 16:35:35 +0000

    ハンドルネームがうまくいかない

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,37 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+devise_for :users, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+#会員側
+ scope module: :public do
+   root to: 'homes#top'
+   get '/about' => 'homes#about'
+   #get '/search', to: 'searches#search'
+   resources :users
+   resources :posts do
+    resource :likes, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
+   end
+ end
+ 
+ 
+devise_for :admin, controllers: {
+  sessions: "admin/sessions"
+}
+#管理者側
+ namespace :admin do
+   root 'homes#top'
+   resources :users
+   resources :posts
+   resources :post_comments
+ end
+  
+
+
+
+
+
 end
